6452ed1aa92d874b7bd4cff0a1496d0b9c258946
TomahawkFMJ <82390412+TomahawkFMJ@users.noreply.github.com>
2021-04-12 16:08:14 +0200

    Add files via upload

---
+++ AmazonTips.py
@@ -0,0 +1,50 @@
+import time                                                                            #assicurarsi di avere un account SiteStripe(gratuito) con amazon
+import telebot                                                                         #pip install pyTelegramBotAPI
+from selenium import webdriver                                                         #pip install selenium/pip install webdriver-manager
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager                               #installare chrome driver piu aggiornati tramite questo link https://chromedriver.chromium.org/
+from selenium.common.exceptions import NoSuchElementException
+API_TOKEN = 'il tuo token del bot telegram'                                            #inserire il token telegram bot
+bot = telebot.TeleBot(API_TOKEN)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    string = str(message.text)
+    if "https://www.amazon.it/" in string.lower():
+        options = webdriver.ChromeOptions()
+        options.headless = True 
+        options.add_argument("user-data-dir=C:\\Users\\il nome dell'utente \\AppData\\Local\\Google\\Chrome\\User Data")       # inserire il perocrso corretto  esempio: "pcdiLuca" del percorso chrome
+        w = webdriver.Chrome(executable_path="percorso dei driver chrome", chrome_options=options)                             # inserire prcorso file driver.exe chrome 
+        w.get(string)
+        try:                              
+            if w.find_element_by_xpath("//div[@class='olp-text-box']").is_displayed():
+                a = w.find_element_by_xpath("//*[@id='priceblock_ourprice']").text
+                b = a.replace(",", ".")
+                c = b.replace("€"," ")
+                d = float(c)
+                offersPanel = w.find_element_by_xpath("//div[@class='olp-text-box']").click()
+                filterList = w.find_element_by_xpath("//*[@id='aod-filter-component']").click()
+                freeShipping = w.find_element_by_xpath("//*[@id='freeShipping']").click()
+                new = w.find_element_by_xpath("//*[@id='new']").click()
+                if w.find_element_by_xpath("//*[@id='aod-price-1']").is_displayed():
+                    e = w.find_element_by_xpath("//*[@id='aod-price-1']").text
+                    f = e.replace("\n", ".")
+                    g = f.replace("€"," ")
+                    h = float(g)
+                    if h < d:          
+                        elem4 = w.find_element_by_xpath("//*[@id='aod-filter-component']").click()
+                        aggiungialcarrello = w.find_element_by_xpath('(//*[@id="a-autoid-2"])[3]').click()
+                        aggiungialcarrello1 = w.find_element_by_xpath("//*[@id='hlb-view-cart']").click()
+                        aggiungialcarrello2 = w.find_element_by_xpath("//span[@class='a-truncate-cut']").click()
+                        w.switch_to.window(w.window_handles[-1])
+                        titolo = w.find_element_by_xpath("//a[@title='Testo']").click()
+                        link = w.find_element_by_xpath("//*[@id='amzn-ss-text-shortlink-textarea']")
+                        bot.reply_to(message, link.text) 
+                        w.quit()
+                    else:
+                        bot.reply_to(message, "A quanto pare l'offerta migliore è quella del tuo link,dannazione!")     #in caso di: offerta svantaggiosa
+                        w.quit()
+        except NoSuchElementException:
+             bot.reply_to(message, "nessuna offerta trovata")   #in caso di: nessuna offerta trovata
+    else:
+        bot.reply_to(message, "link amazon errato")   #in caso di:  link errato 
+bot.polling()
---
+++ README.txt
@@ -0,0 +1,32 @@
+REQUISITI AMAZON TIPS 
+
+1) utilizzare chrome come browser
+
+2)creare un bot telegram in modo semplice con bot father https://t.me/botfather e appuntare il TOKEN https ottenuto
+
+
+
+2) creare un account affiliazione amazon-- https://programma-affiliazione.amazon.it/ (GRATUITO) 
+con il profilo chrome di default (il primo mostrato quando lo si apre)
+
+
+
+3) scaricare i driver chrome piu aggiornati tramite questo link https://chromedriver.chromium.org/
+
+installare le seguenti librerie indicate anche nel file python:
+
+pip install pyTelegramBotAPI
+pip install selenium
+pip install webdriver-manager
+
+
+
+4)per vedere la posizione del profilo di installazione di chrome si puo scrivere sulla barra degli indirizzi 
+chrome://version/
+e sulla voce percorso profilo copiare il testo:  
+C:\Users\User\AppData\Local\Google\Chrome\User Data\Default   
+
+ricordare però di sostituire \ con \\ e di non includere il "\Default" finale 
+al posto di "User" inserire il nome utente del proprio percorso di installazione.
+
+
